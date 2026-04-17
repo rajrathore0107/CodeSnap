c274dca2bdf96a79143966f9c3fea370ca7f2e34
Raj Rathore <rajrathore2902@gmail.com>
2026-04-17 19:47:11 +0530

    fix api base url

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://codesnap-api-jgca.onrender.com',
+  baseURL: 'https://codesnap-api-jgca.onrender.com/api',
 });
 
 api.interceptors.request.use((config) => {
